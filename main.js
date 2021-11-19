60dd2c7eb6fa2ddbb74e932d369de475fef77b71
ashliecodes <ashlieprempeh@hotmail.co.uk>
2021-11-19 09:41:24 +0000

    added if statement

---
+++ main.js
@@ -0,0 +1,10 @@
+let playerMove = "rock";
+let computerMove = "paper";
+
+if (playerMove === "rock"){
+    if (computerMove === "paper"){
+
+    }else if (computerMove === "scissors"){
+
+    } else {}
+}
