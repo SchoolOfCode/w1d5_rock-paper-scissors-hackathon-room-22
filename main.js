9e1cc11ea9bf1dea22cb9abf667e9ac2293feafa
ashliecodes <ashlieprempeh@hotmail.co.uk>
2021-11-19 09:58:19 +0000

    completed task1

--- main.js
+++ main.js
@@ -1,10 +1,38 @@
+//task 1
 let playerMove = "rock";
 let computerMove = "paper";
 
 if (playerMove === "rock"){
     if (computerMove === "paper"){
+        console.log("COMPUTER WINS!!!")
 
     }else if (computerMove === "scissors"){
+        console.log("PLAYER 1 WINS!!!")
 
-    } else {}
+    } else {
+        console.log("DRAW!!!")
+    }
+} else if (playerMove === "paper"){
+    if (computerMove === "paper"){
+        console.log("DRAW!!!")
+
+    }else if (computerMove === "scissors"){
+        console.log("COMPUTER WINS!!!")
+
+    } else {
+        console.log("PLAYER 1 WINS!!!")
+    }
+} else {
+    if (computerMove === "paper"){
+        console.log("PLAYER 1 WINS!!!")
+
+    }else if (computerMove === "scissors"){
+        console.log("DRAW!!!")
+
+    } else {
+        console.log("COMPUTER WINS!!!")
+    }
+
 }
+
+//task 2
